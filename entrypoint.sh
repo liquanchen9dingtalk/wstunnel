5a14a294bb182023912e220b408485e6c3e1d1ab
liquanchen9dingtalk <106719996+liquanchen9dingtalk@users.noreply.github.com>
2023-03-17 16:22:51 +0800

    管理员身份登录准备

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,16 @@ fi
 
 # ${PORT} ${WSPATH}
 
-sed 
-
-
 # 下载 wstool  executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/erebe/wstunnel/releases/download/v5.0/wstunnel-linux-x64 -o /wstunnel
-chmod +x /wstunnel
+sudo curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/erebe/wstunnel/releases/download/v5.0/wstunnel-linux-x64 -o /wstunnel
+sudo chmod +x /wstunnel
 
 # 启动 wstool 服务 
-/wstunnel --server ws://0.0.0.0:33344 &
+sudo /wstunnel --server ws://0.0.0.0:33344 &
 
 # 根据环境变量 配置nginx 配置
-sed -i "s/www_port/${PORT}/g" /etc/nginx/conf.d/default.conf
-sed -i "s/www_wspath/${WSPATH}/g" /etc/nginx/conf.d/default.conf
+sudo sed -i "s/www_port/${PORT}/g" /etc/nginx/conf.d/default.conf
+sudo sed -i "s/www_wspath/${WSPATH}/g" /etc/nginx/conf.d/default.conf
 
 # 启动 nginx 服务
 nginx 
