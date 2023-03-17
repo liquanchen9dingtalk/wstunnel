f1f9a25efe9153db72054d6f9ff3db4a99ee5ab7
liquanchen9dingtalk <106719996+liquanchen9dingtalk@users.noreply.github.com>
2023-03-17 15:59:51 +0800

    启动脚本

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+
+if [ "$WSPATH" = "" ]; then
+export WSPATH="$1"
+fi
+#echo "${WSPATH}"
+
+# ${PORT} ${WSPATH}
+
+sed 
+
+
+# 下载 wstool  executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/erebe/wstunnel/releases/download/v5.0/wstunnel-linux-x64 -o /wstunnel
+chmod +x /wstunnel
+
+# 启动 wstool 服务 
+/wstunnel --server ws://0.0.0.0:33344 &
+
+# 根据环境变量 配置nginx 配置
+sed 's/www_port/${PORT}/' /etc/nginx/conf.d/default.conf
+sed 's/www_wspath/${WSPATH}/' /etc/nginx/conf.d/default.conf
+
+# 启动 nginx 服务
+nginx 
